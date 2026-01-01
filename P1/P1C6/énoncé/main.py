a5216f0b609e047a06cc5387cf1da1e139c854e1
itachi-3 <sunguru123process@gmail.com>
2026-01-01 14:54:24 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits= ["Pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1]= "annanas"
+print(len(fruits))
+fruits.sort()
+print(fruits)
