9f4eaf038d91faf3685e4dba91ebc183f8f72d7d
itachi-3 <sunguru123process@gmail.com>
2026-01-01 09:49:18 +0100

    Update main.py
    réslu

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom="itachi"
+age= 10
+taille= 1,55
+est_etudiant= True
+print(f"je m'appelle {nom} j'ai{age} je mesure {taille} je suis {est_etudiant}")
+type (nom)
+type(age)
+type(taille)
+type(est_etudiant)
