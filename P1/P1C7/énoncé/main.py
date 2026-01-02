f5b9cf9c49f62ec182cd6e3e1da9aad864ceabe2
itachi-3 <sunguru123process@gmail.com>
2026-01-02 07:54:24 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+fruits={"pomme":"rouge","banane":"jaune","orange":"orange"}
+fruits["kiwi"]="vert"
+couleur_banane= fruits["banane"]
+fruits["pomme"]="vert"
+del fruits["banane"]
+print(fruitskeys())
