22e6f4c632dcfdad5f5bb8718709aa979d6c37e0
itachi-3 <sunguru123process@gmail.com>
2026-01-02 10:34:40 +0100

    Update main.py
    pas résolu

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,11 @@
-# Ecrivez votre code ici !
+numéro_1= input("quel est le premier nombre ?")
+numéro_2= input("quel est mon deuxiéme nombre?")
+if not numéro_1.isnumeric() or not numéro_2.isnumeric():
+  print("erreur les deux nombres sont pas des entiers")
+  raise SystemExit("Fin du programme")
+numéro_1= int(numéro_1)
+numéro_2= int(numéro_2)
+opération = input("choisi l'une de ces opérations +,-,:,*")
+if operation not in ["+", "-", "*", "/"]:
+    print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+    raise SystemExit("Fin du programme")
