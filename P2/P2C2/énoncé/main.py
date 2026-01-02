53fcb66be880543407f76f20a8a042dc47a9fbda
itachi-3 <sunguru123process@gmail.com>
2026-01-02 11:08:26 +0100

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,24 @@
-# Ecrivez votre code ici !
+nombres= input("saisissez une suites de nombres séparées par la virgule:")
+listes = nombres.split(",")
+listees_entiers= []
+for x in listes:
+  listes_entiers.append(int(x))
+print(listes_entiers)
+# Exemple de liste d'entiers
+liste_entiers = [1, 2, 3, 4, 5]
+
+# 1. Calculer et afficher la somme
+somme = sum(liste_entiers)
+print("Somme :", somme)
+
+# 2. Calculer et afficher la moyenne
+moyenne = somme / len(liste_entiers)
+print("Moyenne :", moyenne)
+
+# 3. Compter les nombres supérieurs à la moyenne
+compteur = 0
+for x in liste_entiers:
+    if x > moyenne:
+        compteur += 1
+
+print("Nombre de nombres supérieurs à la moyenne :", compteur)
