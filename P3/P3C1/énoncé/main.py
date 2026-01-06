946182c2db83ea89d9b5bcb9ee563dc712526f95
itachi-3 <sunguru123process@gmail.com>
2026-01-06 07:54:42 +0100

    Update main.py
    résolu

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,8 @@
-# Ecrivez votre code ici !
+import operations
+
+
+solution_addition = operation.addition(3,5)
+solution_multiplication = operation.multiplication(8,2)
+
+print(solution_addition)
+print(solution_multiplication)
