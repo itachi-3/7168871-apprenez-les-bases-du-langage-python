354166095f02902b7cc2a91ace5e972a3ce97f54
itachi-3 <sunguru123process@gmail.com>
2026-01-08 08:51:38 +0100

    Update main.py
    pas fini mais fait

--- P3/P3C2/énoncé/main.py
+++ P3/P3C2/énoncé/main.py
@@ -1,3 +1,10 @@
 from bs4 import Beautifulsoup
 
-with open
+with open("index.html","r") as file:
+  soup = beautifulsoup(file,"html.parser")
+
+titre= soup.title.string
+print(titre)
+
+id_h2= soup.find.string(id="h1")
+print(id_h2)
